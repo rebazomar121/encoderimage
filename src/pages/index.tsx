4e2d7973e90298c5a1c49751bc36868917f78acb
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-13 16:07:52 +0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
         <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
+          This  <span className="text-purple-700">application </span> is demo from vercel
         </h1>
       </main>
     </div>
