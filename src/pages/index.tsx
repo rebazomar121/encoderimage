110ec6adf48773a9728fa8c63d6b61e09428c062
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-16 16:46:03 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Input from "../../component/Input"
 
 export default function Home() {
   return (
-    <div className="container flex items-center p-4 mx-auto h-4/6 justify-center">
+    <div className="">
         <Input/>
     </div>
   )
