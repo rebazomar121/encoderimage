de760ae28c0e720e31e6506d9eb5ba90fc2042ba
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-16 17:24:12 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Input from "../../component/Input"
-
+import History  from "../../component/History"
 
 export default function Home() {
   return (
     <div className="">
         <Input/>
+        <History/>
     </div>
   )
 }
