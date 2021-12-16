2239ad07c74bd19de1e5b941e4b758520dd60d16
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-16 16:45:58 +0300

    Update Navbar.tsx

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,8 +1,16 @@
-import React from "react"
-
+import React,{useState} from "react"
+import {GiHamburgerMenu} from "react-icons/gi"
 const Navbar:React.FC = () =>{
-    return <div className="h-9 re-bg-grey">
-        <ul className="flex p-1 text-2xl w-full">
+    // to active mobile navbar 
+    const [navbar_mobile,setnavbar_mobile]=useState(false)
+    // handle to click navbar
+    const HandletoClickMobileNavbar  = () =>{
+        navbar_mobile === true ? setnavbar_mobile(false) : setnavbar_mobile(true); 
+    } 
+
+    return <div className="bg-gray-300">
+        <button onClick={HandletoClickMobileNavbar} className="p-6 text-2xl"> <GiHamburgerMenu/> </button>
+        <ul className="block p-1 text-xl w-full">
             <li className="m-7" >Home</li>
             <li className="m-7">About App</li>
             <li className="m-7">Feedback</li>
