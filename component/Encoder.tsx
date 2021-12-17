823b7bd69aa6139e4f02c44ea1937d8bf5b78a54
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-17 23:17:25 +0300

    history and encoder

---
+++ component/Encoder.tsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+
+function Encoder() {
+    const [file,setfile]= useState([])
+
+    const getdata = () => {
+
+    }
+}
+
--- component/History.tsx
+++ component/History.tsx
@@ -2,8 +2,11 @@ import React from "react"
 
 
 const History:React.FC = () => {
-    return <div>
-        <h1>History</h1>
+    return <div className="p-7">
+        <h1 className="text-2xl">History</h1>
+        <ul className="ml-7 mt-7">
+            <li>Nothing in History</li>
+        </ul>
     </div>
 }
 export default History
