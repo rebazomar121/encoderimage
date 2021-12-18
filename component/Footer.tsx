32fed95222aee07f9963daf99258353b452194f7
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-18 09:09:36 +0300

    some note
    add only some note

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Link from "next/link"
 import {AiFillGithub}from "react-icons/ai"
 const Footer:React.FC = () =>{
-    return <div className="text-center  fixed inset-x-0 bottom-0 bg-gray-300">
+    return <div className="text-center text-2xl  fixed inset-x-0 bottom-0 bg-gray-300">
        <Link href="https://github.com/rebazomar121"><p className="cursor-pointer">Github : rebazomar121</p></Link> 
     </div>
 }
--- component/Input.tsx
+++ component/Input.tsx
@@ -6,7 +6,6 @@ import { UserContext } from '../context/context'
 const Input: React.FC = () => {
     // UserContext
     const AppState:any= useContext(UserContext)
-
     const EncoderFileBase64 = (file) => {
         // make reader to reader exernal
         console.log("hare")
@@ -23,7 +22,6 @@ const Input: React.FC = () => {
             }
         }
     }
-
     const Handlegetdata =(mydata)=>{
         console.log(mydata)
         AppState.setsendfile(mydata)
@@ -32,8 +30,10 @@ const Input: React.FC = () => {
     return <div className='md:grid grid-cols-2'>
          {/* this is uplode */} 
         <div className=" text-center p-7 md:p-32 ">
-            <p className='text-center m-2'>Uplode Your file hare</p>
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"> Uplode</button>
+            <p className='text-center m-2'>You can uplode file or imgae</p>
+            <p className='text-red-500'>Note</p>
+            <p>your file and imgae are storing on your device</p>
+            <p>this is not encrypt it's just decode , so becarfule do not share your decode string with other</p>
         </div>  
         {/* this is link you can uplode in hare */} 
         <div className="py-20 bg-white px-2 -order-1">
