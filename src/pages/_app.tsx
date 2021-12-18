acb414b54ac1f7f9f8fde1da3384a09ca89185dc
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-18 08:49:58 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { AppProps } from 'next/app'
 import Layout from "../../component/Layout"
 import Navbar from '../../component/Navbar'
+import UserProvider from "../../context/context"
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <UserProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </UserProvider>
     </>
   )
 }
