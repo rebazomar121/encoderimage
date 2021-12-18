86f9c9c434ef33dc31efc20e40d776ea1f0a3529
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-18 10:13:41 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 module.exports = {
+  important: true,
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
   purge: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/pages/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
   },
-  variants: ["responsive", "group-hover", "hover", "focus"],
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
   plugins: [
     require('@themesberg/flowbite/plugin')
-],
+],  future: {
+  purgeLayersByDefault: true,
+}
 }
