7d706427f7dc4dfb827cae0c700c5e890951b253
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2021-12-20 18:57:31 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Input from "../../component/Input"
-import History  from "../../component/History"
-
+import History  from "../../component/LastItem"
+import ShowData from "../../component/ShowData"
 export default function Home() {
   return (
     <div className="">
         <Input/>
+        <ShowData/>
         <History/>
     </div>
   )
